c698de312fef9b97287960affe354f83d900e2e7
АсмановОлексійАлійов <asmanov.a@gmail.com>
2024-02-18 11:50:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.S3;
+using Amazon.S3.Transfer;
+using Amazon.Runtime;
+using Amazon.S3.Model;
+
+
+class S3Uploader
+{
+    static async Task Main(string[] args)
+    {
+        BasicAWSCredentials credentials = new BasicAWSCredentials("AKIAQ3EGP4UWFONW466S", "Nm/dvNTuQtmuZU00/eqiCFnWA475euLnHp9vMZQH");
+        IAmazonS3 client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.USEast1);
+        string bucketName = "asmanovbucket";
+        string keyName = "";
+        string filePath = @"C:\Users\АсмановОлексійАлійов\Downloads\20231105_152028.jpg";
+
+        try
+        {
+            // Upload the file to Amazon S3
+            TransferUtility fileTransferUtility = new TransferUtility(client);
+            fileTransferUtility.Upload(filePath, bucketName, keyName);
+            Console.WriteLine("Upload completed!");
+            fileTransferUtility.Download(@"C:\Users\АсмановОлексійАлійов\Downloads\!!!!20231105_152058.jpg", bucketName, "20231105_152058.jpg");
+            Console.WriteLine("Download completed");
+        }
+        catch (AmazonS3Exception e)
+        {
+            Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
+        }
+        
+    }
+}
